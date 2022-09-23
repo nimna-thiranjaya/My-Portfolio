ca9a1f2e4135b799473617f442c8bae870c8db33
Nimna Thiranjaya <nimnathiranjya523@gmail.com>
2022-09-23 13:39:55 +0530

    header change

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ export const headerData = {
   name: "Nimna Thiranjaya",
   title: "Web Developer",
   desciption:
-    '"The expert in anything was once a beginer". I am a full stack developer who has keen interest in developing backend . I mostly prefer Spring boot, Java, NodeJs and Laravel and also I have an experience in web application frontend development with reactJs, HTML, CSS, and Javascript',
+    '"The expert in anything was once a beginer". I am a full stack developer who has keen interest in developing backend . I mostly prefer Spring boot, python, NodeJs and Laravel and also I have an experience in web application frontend development with reactJs, HTML, CSS, and Javascript',
   image:
     "https://res.cloudinary.com/dx1pvvqg7/image/upload/v1663695180/portfoliyo/277748385_1356754131404095_4749045328266777219_n_a7n6zm.ico",
   resumePdf: resume,
